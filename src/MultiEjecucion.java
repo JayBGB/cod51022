e08cab9446b1b54b381b98770d29b6396c88253a
Jay <bgarciabarrera@danielcastelao.org>
2022-10-14 09:03:22 +0200

    v1.0

--- src/MultiEjecucion.java
+++ src/MultiEjecucion.java
@@ -1,10 +1,14 @@
+/**
+ * @version 1.0
+ */
+
 import javax.swing.*;
 
 public class MultiEjecucion {
 
     public static void main (String [] args) {
 
-        JOptionPane.showMessageDialog(null,"Hello World!");
+        JOptionPane.showMessageDialog(null,"Hello World! v1.0");
 
     }
 }
